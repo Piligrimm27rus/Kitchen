253be502fdfb1e73c0361b99f1be2bfceb8c8801
Vatulin Evgeny <vatulin.evgeny@sogaz.ru>
2021-11-02 18:42:11 +1000

    deleted unnecessary files

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -1,17 +1,14 @@
 import React from 'react';
-
+import '../../App.css';
 
 class Header extends React.Component {
-    constructor(props) {
-        super(props);
-    }    
-
-    render(){
-        return[
-            <p> Количество кликов: {this.props.clicks}</p>,
-            <button onClick={() => this.props.clickbtn()}>Прибавить 1</button>
-        ]
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
     }
-} 
+}
 
 export default Header;
---
+++ my-app/src/components/pages/main-page/main-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../App.css';
+
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default MainPage;
